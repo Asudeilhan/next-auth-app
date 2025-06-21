c810a1c99dd96c79a7306f421b45d2c96b256331
Asudeilhan <asudeilhan12@gmail.com>
2025-06-21 18:09:47 +0300

    Initial project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-4xl text-blue-600 font-bold">
+        Tailwind Başarıyla Çalışıyor 🎉
+      </h1>
+    </div>
+  );
+}
